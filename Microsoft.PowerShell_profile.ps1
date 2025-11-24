655d13ecd2de4c0aaf3b8ca03b327399da88ed42
pipinfitriadi <pipinfitriadi@gmail.com>
2025-11-24 05:40:21 +0000

    feat: Create branch for vscode windows env

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+# Copyright (C) Pipin Fitriadi - All Rights Reserved
+
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 24 November 2025
+
+& oh-my-posh init pwsh --config .\.oh-my-posh\spaceship.omp.json | Invoke-Expression
