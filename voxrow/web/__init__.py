7d133d782eb3b1f71e12fe40798a2a85298e1d6b
Pipin Fitriadi <pipinfitriadi@gmail.com>
2025-12-28 05:23:31 +0000

    fix: Implement compress and minify html

--- voxrow/web/__init__.py
+++ voxrow/web/__init__.py
@@ -7,11 +7,14 @@
 # Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 9 December 2025
 
 from pathlib import Path
+from typing import Callable
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from minify_html import minify
 
 ROOT_DIR: Path = Path("voxrow") / "web"
 app: FastAPI = FastAPI(
@@ -22,6 +25,37 @@ templates: Jinja2Templates = Jinja2Templates(
     directory=ROOT_DIR / "templates",
 )
 
+
+@app.middleware("http")
+async def minify_html_middleware(request: Request, call_next: Callable) -> Response:
+    response: Response = await call_next(request)
+
+    if "text/html" in response.headers.get("content-type", ""):
+        response_body: bytes = b""
+
+        async for chunk in response.body_iterator:
+            response_body += chunk
+
+        minified_content: bytes = minify(
+            response_body.decode("utf-8"),
+            allow_noncompliant_unquoted_attribute_values=True,
+            minify_css=True,
+            minify_js=True,
+        ).encode("utf-8")
+
+        # Update the response body and content length
+        response.headers["content-length"] = str(len(minified_content))
+
+        return HTMLResponse(
+            content=minified_content,
+            status_code=response.status_code,
+            headers=dict(response.headers),
+        )
+
+    return response
+
+
+app.add_middleware(GZipMiddleware)
 app.mount(
     "/files",
     StaticFiles(
