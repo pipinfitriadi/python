dbf50e10a7c5af119dd6ec47d0488303d54fc6b7
Pipin Fitriadi <pipinfitriadi@gmail.com>
2025-12-28 07:38:58 +0000

    fix: create inflation.json if not exists
    Implement basedd on notebook

--- voxrow/web/__init__.py
+++ voxrow/web/__init__.py
@@ -6,9 +6,18 @@
 # Proprietary and confidential
 # Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 9 December 2025
 
+import html
+import json
+from calendar import monthrange
+from io import StringIO
+from os import getenv
 from pathlib import Path
 from typing import Callable
 
+import httpx
+import lxml.html
+import numpy as np
+import pandas as pd
 from fastapi import FastAPI, Request, Response
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import FileResponse, HTMLResponse
@@ -18,6 +27,7 @@ from minify_html import minify
 
 ENCODING: str = "utf-8"
 ROOT_DIR: Path = Path("voxrow") / "web"
+STATIC_DIR: Path = ROOT_DIR / "static"
 TITLE: str = "VOXROW"
 app: FastAPI = FastAPI(
     docs_url=None,
@@ -59,20 +69,105 @@ async def minify_html_middleware(request: Request, call_next: Callable) -> Respo
 app.add_middleware(GZipMiddleware)
 app.mount(
     "/files",
-    StaticFiles(
-        directory=ROOT_DIR / "static",
-    ),
+    StaticFiles(directory=STATIC_DIR),
     name="static",
 )
 
 
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
-    return FileResponse(ROOT_DIR / "static" / "favicon.svg")
+    return FileResponse(STATIC_DIR / "favicon.svg")
 
 
 @app.get("/", response_class=HTMLResponse, include_in_schema=False)
 async def root(request: Request) -> dict:
+    json_file: Path = STATIC_DIR / "inflation.json"
+
+    if not json_file.is_file():
+        resp: httpx.Response = httpx.get(
+            "https://webapi.bps.go.id/v1/api/view/domain/{DOMAIN}/model/{MODEL}/lang/{LANG}/id/{ID}/key/{KEY}/".format(
+                KEY=getenv("BPS_KEY"),
+                LANG="ind",
+                DOMAIN="0000",  # Pusat
+                MODEL="statictable",  # Static Table
+                ID=915,  # Tingkat Inflasi Harga Konsumen Nasional Tahunan (Y-on-Y)
+            )
+        )
+        data: dict = resp.json()["data"]
+        title: str = lxml.html.fromstring(data["title"]).text.strip()
+        df: pd.DataFrame = pd.read_html(
+            io=StringIO(html.unescape(data["table"])),
+            header=2,
+            index_col=0,
+            decimal=",",
+            thousands=".",
+            flavor="lxml",
+        )[0][:12]
+        df_flat_table: pd.DataFrame = (
+            df.reset_index()
+            .rename(columns={"index": "Bulan"})
+            .melt(
+                id_vars="Bulan",
+                var_name="Tahun",
+                value_name="inflation",
+            )
+        )
+        df_flat_table["date"] = (
+            pd.concat(
+                [
+                    df_flat_table["Tahun"].astype(int),
+                    df_flat_table["Bulan"].map(
+                        {
+                            "Januari": 1,
+                            "Februari": 2,
+                            "Maret": 3,
+                            "April": 4,
+                            "Mei": 5,
+                            "Juni": 6,
+                            "Juli": 7,
+                            "Agustus": 8,
+                            "September": 9,
+                            "Oktober": 10,
+                            "November": 11,
+                            "Desember": 12,
+                        }
+                    ),
+                ],
+                axis=1,
+            )
+            .apply(
+                lambda row: pd.to_datetime(
+                    "{year}-{month}-{day}".format(
+                        year=row["Tahun"],
+                        month=row["Bulan"],
+                        day=monthrange(row["Tahun"], row["Bulan"])[1],
+                    )
+                ),
+                axis=1,
+            )
+            .dt.date
+        )
+        df_flat_table["inflation"] = df_flat_table["inflation"].astype(float)
+
+        del df_flat_table["Bulan"]
+        del df_flat_table["Tahun"]
+
+        df_flat_table.set_index("date", inplace=True)
+
+        with open(json_file, "w") as file:
+            json.dump(
+                dict(
+                    title=title,
+                    data=(
+                        df_flat_table.replace({np.nan: None})
+                        .reset_index()
+                        .to_dict("records")
+                    ),
+                ),
+                file,
+                default=str,
+            )
+
     return templates.TemplateResponse(
         request=request,
         name="index.html",
