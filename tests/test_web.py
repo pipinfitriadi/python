40f93c4b2afce2cc61dfed0bc2b8de41a59f9a8b
Pipin Fitriadi <pipinfitriadi@gmail.com>
2026-01-16 09:57:28 +0000

    fix: Full coverage for api endpoint post /bps/inflation

--- tests/test_web.py
+++ tests/test_web.py
@@ -6,13 +6,15 @@
 # Proprietary and confidential
 # Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 9 December 2025
 
-# import json
+import json
 from http import HTTPStatus
 from pathlib import Path
+from typing import Callable
+from unittest.mock import MagicMock
 
 from fastapi.testclient import TestClient
 from httpx import Response
-from pytest_mock import MockerFixture
+from pytest import MonkeyPatch, fixture
 
 from voxrow.core.domain.value_objects import ContentType
 from voxrow.data.domain.value_objects import Boto3Credential
@@ -21,7 +23,9 @@ from voxrow.web.entrypoint import app, get_settings
 
 # Constants
 TEST_FILES_DIR: Path = Path("tests") / "files"
-TEST_FILE_INFLATION: Path = TEST_FILES_DIR / "datalake" / "bps" / "inflation.json"
+TEST_FILE_INFLATION: str = (
+    TEST_FILES_DIR / "datalake" / "bps" / "inflation.json"
+).read_text()
 
 
 def fake_get_settings() -> Settings:
@@ -40,6 +44,36 @@ app.dependency_overrides[get_settings] = fake_get_settings
 client: TestClient = TestClient(app)
 
 
+# Mocks
+@fixture
+def mock_extract_inflation_bps(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "voxrow.data.adapters.ports.boto3.client",
+        lambda *args, **kwargs: MagicMock(
+            put_object=MagicMock(return_value=None),
+            get_object=MagicMock(
+                return_value=dict(
+                    Body=MagicMock(
+                        read=MagicMock(
+                            return_value=MagicMock(
+                                decode=MagicMock(
+                                    return_value=TEST_FILE_INFLATION,
+                                )
+                            )
+                        )
+                    ),
+                )
+            ),
+        ),
+    )
+    monkeypatch.setattr(
+        "voxrow.core.adapters.ports.httpx.get",
+        lambda *args, **kwargs: MagicMock(
+            json=MagicMock(return_value=json.loads(TEST_FILE_INFLATION)),
+        ),
+    )
+
+
 # Tests
 def test_favicon() -> None:
     response: Response = client.get("/favicon.ico")
@@ -49,15 +83,17 @@ def test_favicon() -> None:
     assert response.text != ""
 
 
-def test_root(mocker: MockerFixture) -> None:
-    # mocker_response: mocker.Mock = mocker.Mock()
-    # mocker_response.json.return_value = json.loads(TEST_FILE_INFLATION.read_text())
-    # mocker.patch("voxrow.core.adapters.ports.httpx.get", return_value=mocker_response)
+def test_extract_inflation_bps(mock_extract_inflation_bps: Callable) -> None:
+    response: Response = client.post("/bps/inflation")
 
-    # mocker.patch.object(Path, "write_text", side_effect=None)
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers["content-type"] == ContentType.json
+    assert response.text == "null"
 
+
+def test_root() -> None:
     response: Response = client.get("/")
 
     assert response.status_code == HTTPStatus.OK
     assert ContentType.html in response.headers["content-type"]
-    assert response.text != ""
+    assert "Inflasi" in response.text
