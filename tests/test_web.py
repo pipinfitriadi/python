f32c76e91dfdeb7ea73b7cf0ebc61f104092328a
Pipin Fitriadi <pipinfitriadi@gmail.com>
2026-01-26 22:53:21 +0000

    fix: Implement voxrow.web.entrypoints.typer and change pyproject.toml's script voxrow.web to this CLI

--- tests/test_web.py
+++ tests/test_web.py
@@ -16,12 +16,13 @@ from botocore.client import BaseClient
 from fastapi.testclient import TestClient
 from httpx import Response
 from pytest import MonkeyPatch, fixture
+from typer.testing import CliRunner, Result
 
 from voxrow.core.domain.value_objects import ContentType
 from voxrow.data.domain.value_objects import Boto3Credential
-from voxrow.web.domain.domain_services import get_settings
+from voxrow.web.domain.domain_services import get_fastapi_settings
 from voxrow.web.domain.value_objects import Settings
-from voxrow.web.entrypoints.fastapi import app
+from voxrow.web.entrypoints import fastapi, typer
 
 # Constants
 TEST_FILES_DIR: Path = Path("tests") / "files"
@@ -30,6 +31,7 @@ TEST_FILE_INFLATION: str = (
     TEST_DATALAKE_DIR / "webapi.bps.go.id" / "inflation.json"
 ).read_text()
 TEST_KEY: str = "123"
+TEST_DATE: str = "2026-01-19"
 
 
 def fake_get_settings() -> Settings:
@@ -43,18 +45,6 @@ def fake_get_settings() -> Settings:
         cron_secret=TEST_KEY,
         decodo_web_scraping_token=TEST_KEY,
     )
-
-
-# Variables
-app.dependency_overrides[get_settings] = fake_get_settings
-
-
-@app.get("/test")
-async def trigger_http_500() -> int:
-    return 1 / 0
-
-
-client: TestClient = TestClient(app, raise_server_exceptions=False)
 
 
 # Mocks
@@ -90,6 +80,10 @@ def mock_extract_bps_inflation(monkeypatch: MonkeyPatch) -> None:
 @fixture
 def mock_extract_idx_stock_summary(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr(
+        "voxrow.data.services.handlers.sleep",
+        MagicMock(),
+    )
+    monkeypatch.setattr(
         "voxrow.data.services.unit_of_work.client",
         lambda *args, **kwargs: MagicMock(
             spec=BaseClient,
@@ -115,80 +109,143 @@ def mock_extract_idx_stock_summary(monkeypatch: MonkeyPatch) -> None:
 
 
 # Tests
-def test_trigger_http_500() -> None:
-    response: Response = client.get("/test")
-    text: str = response.text
+class TestFastAPI:
+    client: TestClient
 
-    assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert ContentType.html in response.headers["content-type"]
-    assert "Internal Server Error" in text
-    assert str(HTTPStatus.INTERNAL_SERVER_ERROR) in text
+    @fixture(autouse=True)
+    def setup(self) -> None:
+        fastapi.app.dependency_overrides[get_fastapi_settings] = fake_get_settings
+
+        @fastapi.app.get("/test")
+        async def trigger_http_500() -> int:
+            return 1 / 0
+
+        self.client = TestClient(fastapi.app, raise_server_exceptions=False)
+
+    def test_trigger_http_500(self) -> None:
+        response: Response = self.client.get("/test")
+        text: str = response.text
+
+        assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
+        assert ContentType.html in response.headers["content-type"]
+        assert "Internal Server Error" in text
+        assert str(HTTPStatus.INTERNAL_SERVER_ERROR) in text
+
+    def test_openapi_json(self) -> None:
+        response: Response = self.client.get("/openapi.json")
+
+        assert response.status_code == HTTPStatus.NOT_FOUND
+        assert ContentType.html in response.headers["content-type"]
+
+    def test_docs(self) -> None:
+        response: Response = self.client.get("/docs")
+
+        assert response.status_code == HTTPStatus.NOT_FOUND
+        assert ContentType.html in response.headers["content-type"]
+
+    def test_redoc(self) -> None:
+        response: Response = self.client.get("/redoc")
+
+        assert response.status_code == HTTPStatus.NOT_FOUND
+        assert ContentType.html in response.headers["content-type"]
+
+    def test_favicon(self) -> None:
+        response: Response = self.client.get("/favicon.ico")
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.headers["content-type"] == ContentType.svg
+        assert response.text != ""
+
+    def test_extract_bps_inflation(self, mock_extract_bps_inflation: Callable) -> None:
+        response: Response = self.client.get(
+            "/bps/inflation",
+            headers=dict(Authorization="Bearer abc"),
+        )
+        text: str = response.text
+
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert ContentType.html in response.headers["content-type"]
+        assert "Invalid or expired token" in text
+        assert str(HTTPStatus.UNAUTHORIZED) in text
+
+        response = self.client.get(
+            "/bps/inflation",
+            headers=dict(Authorization=f"Bearer {TEST_KEY}"),
+        )
+
+        assert response.status_code == HTTPStatus.NO_CONTENT
+        assert response.text == ""
+
+    def test_extract_idx_stock_summary(
+        self, mock_extract_idx_stock_summary: Callable
+    ) -> None:
+        response: Response = self.client.get(
+            "/idx/stock-summary",
+            headers=dict(Authorization=f"Bearer {TEST_KEY}"),
+            params=dict(date=TEST_DATE),
+        )
+
+        assert response.status_code == HTTPStatus.NO_CONTENT
+        assert response.text == ""
+
+    def test_root(self) -> None:
+        response: Response = self.client.get("/")
+
+        assert response.status_code == HTTPStatus.OK
+        assert ContentType.html in response.headers["content-type"]
+        assert "Inflasi" in response.text
 
 
-def test_openapi_json() -> None:
-    response: Response = client.get("/openapi.json")
+class TestTyper:
+    runner: CliRunner
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert ContentType.html in response.headers["content-type"]
+    @fixture(autouse=True)
+    def setup(self) -> None:
+        self.runner = CliRunner()
 
+    @fixture
+    def mock_get_typer_settings(self, monkeypatch: MonkeyPatch) -> None:
+        monkeypatch.setattr(
+            "voxrow.web.domain.domain_services.get_typer_settings",
+            fake_get_settings,
+        )
 
-def test_docs() -> None:
-    response: Response = client.get("/docs")
+    def test_extract_bps_inflation(
+        self,
+        mock_get_typer_settings: Callable,
+        mock_extract_bps_inflation: Callable,
+    ) -> None:
+        result: Result = self.runner.invoke(typer.app, ["extract-bps-inflation"])
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert ContentType.html in response.headers["content-type"]
+        assert result.exit_code == 0
 
+    def test_extract_idx_stock_summary(
+        self,
+        mock_get_typer_settings: Callable,
+        mock_extract_idx_stock_summary: Callable,
+    ) -> None:
+        result: Result = self.runner.invoke(
+            typer.app,
+            [
+                "extract-idx-stock-summary",
+                "--start-date",
+                TEST_DATE,
+                "--end-date",
+                TEST_DATE,
+            ],
+        )
 
-def test_redoc() -> None:
-    response: Response = client.get("/redoc")
+        assert result.exit_code == 0
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert ContentType.html in response.headers["content-type"]
+        result = self.runner.invoke(
+            typer.app,
+            [
+                "extract-idx-stock-summary",
+                "--start-date",
+                "2026-01-20",
+                "--end-date",
+                TEST_DATE,
+            ],
+        )
 
-
-def test_favicon() -> None:
-    response: Response = client.get("/favicon.ico")
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.headers["content-type"] == ContentType.svg
-    assert response.text != ""
-
-
-def test_extract_bps_inflation(mock_extract_bps_inflation: Callable) -> None:
-    response: Response = client.get(
-        "/bps/inflation",
-        headers=dict(Authorization="Bearer abc"),
-    )
-    text: str = response.text
-
-    assert response.status_code == HTTPStatus.UNAUTHORIZED
-    assert ContentType.html in response.headers["content-type"]
-    assert "Invalid or expired token" in text
-    assert str(HTTPStatus.UNAUTHORIZED) in text
-
-    response = client.get(
-        "/bps/inflation",
-        headers=dict(Authorization=f"Bearer {TEST_KEY}"),
-    )
-
-    assert response.status_code == HTTPStatus.NO_CONTENT
-    assert response.text == ""
-
-
-def test_extract_idx_stock_summary(mock_extract_idx_stock_summary: Callable) -> None:
-    response: Response = client.get(
-        "/idx/stock-summary",
-        headers=dict(Authorization=f"Bearer {TEST_KEY}"),
-        params=dict(date="2026-01-19"),
-    )
-
-    assert response.status_code == HTTPStatus.NO_CONTENT
-    assert response.text == ""
-
-
-def test_root() -> None:
-    response: Response = client.get("/")
-
-    assert response.status_code == HTTPStatus.OK
-    assert ContentType.html in response.headers["content-type"]
-    assert "Inflasi" in response.text
+        assert result.exit_code == 0
