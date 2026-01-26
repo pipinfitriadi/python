4a1abf2719a340fe5cff4eef3a5036776b4a9423
Pipin Fitriadi <pipinfitriadi@gmail.com>
2026-01-26 11:26:55 +0000

    fix: Init Typer CLI for voxrow.web terminal commands

--- voxrow/web/domain/value_objects.py
+++ voxrow/web/domain/value_objects.py
@@ -6,13 +6,22 @@
 # Proprietary and confidential
 # Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 31 December 2025
 
+from importlib.metadata import PackageMetadata, metadata, version
 from pathlib import Path
 
-from pydantic import SecretStr
+from pydantic import EmailStr, HttpUrl, SecretStr
 
 from ...data.domain import value_objects
 
 # Constants
+TITLE: str = "VOXROW"
+DISTRIBUTION_NAME: str = "voxrow.web"
+VERSION: str = version(DISTRIBUTION_NAME)
+METADATA: PackageMetadata = metadata(DISTRIBUTION_NAME)
+LICENSE: str = METADATA.get("license")
+AUTHOR: str = METADATA.get("Author")
+EMAIL: EmailStr = METADATA.get("Author-email")
+URL: HttpUrl = HttpUrl(METADATA.get_all("Project-URL")[0].split(", ")[1])
 ROOT_DIR: Path = Path("voxrow") / "web"
 STATIC_DIR: Path = ROOT_DIR / "static"
 
