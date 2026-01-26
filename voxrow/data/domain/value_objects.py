fdad12db817f4aafe2b5d21c7dee1608a6fa84a4
Pipin Fitriadi <pipinfitriadi@gmail.com>
2026-01-26 07:14:03 +0000

    fix: Decouples extract_idx_stock_summary and extract_bps_inflation into voxrow.data.services.handlers

--- voxrow/data/domain/value_objects.py
+++ voxrow/data/domain/value_objects.py
@@ -6,11 +6,14 @@
 # Proprietary and confidential
 # Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 14 January 2026
 
+from datetime import date, datetime
 from pathlib import Path
+from typing import Annotated, TypeAlias
 from zoneinfo import ZoneInfo
 
-from pydantic import HttpUrl, SecretStr
+from pydantic import Field, HttpUrl, SecretStr
 from pydantic.dataclasses import dataclass
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from ...core.domain.value_objects import (
     ContentEncoding,
@@ -21,6 +24,11 @@ from ...core.domain.value_objects import (
 
 # Constants
 TIME_ZONE: ZoneInfo = ZoneInfo("Asia/Jakarta")
+
+Date: TypeAlias = Annotated[
+    date,
+    Field(default_factory=lambda: datetime.now(tz=TIME_ZONE).date),
+]
 
 
 @dataclass(frozen=True)
@@ -42,3 +50,14 @@ class Boto3Source(Source):
 class Boto3Destination(Destination, Boto3Source):
     content_type: ContentType
     content_encoding: ContentEncoding | None = None
+
+
+class Settings(BaseSettings):
+    model_config: SettingsConfigDict = SettingsConfigDict(
+        env_nested_delimiter="__",
+        frozen=True,
+    )
+
+    bps_key: SecretStr
+    cloudflare_r2: Boto3Credential
+    decodo_web_scraping_token: SecretStr
---
+++ voxrow/data/services/handlers.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+# Copyright (C) Pipin Fitriadi - All Rights Reserved
+
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 26 January 2026
+
+from calendar import monthrange
+from datetime import date, datetime
+from http import HTTPMethod
+from pathlib import Path
+
+from pydantic import validate_call
+
+from ...core.domain import value_objects
+from ...core.services import handlers, unit_of_work
+from ..domain import domain_services
+from ..domain.value_objects import (
+    TIME_ZONE,
+    Boto3Destination,
+    Boto3Source,
+    Date,
+    Settings,
+)
+from .unit_of_work import Boto3DataUnitOfWork
+
+
+@validate_call
+async def extract_bps_inflation(settings: Settings) -> None:
+    BUCKET: str = "datalake"
+    WEB_DOMAIN: str = "webapi.bps.go.id"
+    now: datetime = datetime.now(tz=TIME_ZONE)
+    filename: date = date(
+        year=now.year,
+        month=now.month,
+        day=monthrange(now.year, now.month)[1],
+    )
+    uow_boto3: Boto3DataUnitOfWork = Boto3DataUnitOfWork(settings.cloudflare_r2)
+    datalake_key: Path = handlers.etl(
+        sources=(
+            unit_of_work.HttpxDataUnitOfWork()(
+                source=value_objects.HttpxSource(
+                    url="https://{WEB_DOMAIN}/v1/api/view/domain/{DOMAIN}/model/{MODEL}/lang/{LANG}/id/{ID}/key/{KEY}/".format(
+                        WEB_DOMAIN=WEB_DOMAIN,
+                        KEY=settings.bps_key.get_secret_value(),
+                        LANG="ind",
+                        DOMAIN="0000",  # Pusat
+                        MODEL="statictable",  # Static Table
+                        # Tingkat Inflasi Harga Konsumen Nasional Tahunan (Y-on-Y)
+                        ID=915,
+                    )
+                )
+            ),
+        ),
+        destination=uow_boto3(
+            destination=Boto3Destination(
+                bucket=BUCKET,
+                key=f"{WEB_DOMAIN}/inflation/{filename}.json.gz",
+                content_type=value_objects.ContentType.json,
+                content_encoding=value_objects.ContentEncoding.gzip,
+            )
+        ),
+    )
+
+    handlers.etl(
+        sources=(
+            uow_boto3(
+                source=Boto3Source(
+                    bucket=BUCKET,
+                    key=datalake_key,
+                )
+            ),
+        ),
+        destination=uow_boto3(
+            destination=Boto3Destination(
+                bucket="web",
+                key="inflation.json",
+                content_type=value_objects.ContentType.json,
+            )
+        ),
+        transform=domain_services.inflation_bps_to_datamart,
+    )
+
+
+@validate_call
+async def extract_idx_stock_summary(settings: Settings, date: Date) -> None:
+    if date.strftime("%a") not in ("Sat", "Sun"):
+        WEB_DOMAIN: str = "idx.co.id"
+
+        with unit_of_work.HttpxDataUnitOfWork()(
+            source=value_objects.HttpxSource(
+                url="https://scraper-api.decodo.com/v2/scrape",
+                method=HTTPMethod.POST,
+                headers={
+                    "Authorization": "Basic {decodo_token}".format(
+                        decodo_token=settings.decodo_web_scraping_token.get_secret_value()
+                    ),
+                    "Accept": value_objects.ContentType.json,
+                    "Content-Type": value_objects.ContentType.json,
+                },
+                json=dict(
+                    url="https://{WEB_DOMAIN}/primary/TradingSummary/GetStockSummary?date={DATE}".format(
+                        WEB_DOMAIN=WEB_DOMAIN,
+                        DATE=date.strftime("%Y%m%d"),
+                    ),
+                    successful_status_codes=[200],
+                ),
+                timeout=60,
+            )
+        ) as uow:
+            data: value_objects.Data = domain_services.decodo_web_scraping_parsed(
+                uow.source_port.extract(source=uow.source_domain)
+            )
+
+        if data["data"]:
+            handlers.etl(
+                sources=(data,),
+                destination=Boto3DataUnitOfWork(settings.cloudflare_r2)(
+                    destination=Boto3Destination(
+                        bucket="datalake",
+                        key=f"{WEB_DOMAIN}/GetStockSummary/{date}.json.gz",
+                        content_type=value_objects.ContentType.json,
+                        content_encoding=value_objects.ContentEncoding.gzip,
+                    )
+                ),
+            )
